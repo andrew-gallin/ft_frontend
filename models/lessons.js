436990507271399332596a1a5a8c3e29a216f60a
Andrew Gallin <andrewgallin@gmail.com>
2019-01-02 18:17:02 -0500

    mongoose schema

---
+++ models/lessons.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const lessonSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author:{
+    type: String,
+    required: true
+  },
+  description:{
+    type: String,
+    required: true
+  },
+  language:{
+    type: String,
+    required: true
+  },
+  difficulty:{
+    type: Number,
+    required: true
+  },
+  createdOn:{
+    type: Date,
+    required: true
+  }
+});
+
+mongoose.model('Lesson', lessonSchema)
